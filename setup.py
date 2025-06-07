ac8bb87ec4a5c9220dc09647b13d77985373c1cc
dinesh-jangir <dineshjangir887766@gmail.com>
2025-06-07 14:30:28 +0530

    Basic Structure Completed

--- setup.py
+++ setup.py
@@ -16,12 +16,12 @@ def get_requirements(file_path:str)->List[str]:
     
     return requirements
 
-
 setup(
-name="mlproject",
-version="0.0.1",
-author="Dinesh",
-author_email="dinesh887766@gmail.com",
+name='mlproject',
+version='0.0.1',
+author='Krish',
+author_email='krishnaik06@gmail.com',
 packages=find_packages(),
-install_requires=get_requirements("requirement.txt")
+install_requires=get_requirements('requirements.txt')
+
 )
